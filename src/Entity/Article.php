cea51a62fccae9aff643e953fbd86727ad4429be
Julien Dihal <juliendihal971@gmail.com>
2021-07-06 15:35:00 +0200

    db

---
+++ src/Entity/Article.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class Article
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createatt;
+
+
+}
