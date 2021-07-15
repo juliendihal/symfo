1728a81e97f61e3e7e5afaeaff04c2ef5d067181
Julien Dihal <juliendihal971@gmail.com>
2021-07-15 14:55:15 +0200

    tag category form

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +19,17 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content')
+            ->add('content' , TextareaType::class)
             ->add('createatt', DateType::class ,[
                 'widget'=> 'single_text'
+            ])
+            ->add('category', EntityType::class , [
+                'class'=> Category::class,
+                'choice_label'=>"title"
+            ])
+            ->add('tag', EntityType::class, [
+                'class'=>Tag::class,
+                'choice_label'=> 'title'
             ])
             ->add('isPublished')
             ->add('submit', SubmitType::class)
